b15cfb0901c119b546f0b1d524c4376d43da4a03
haoyus2 <haoyus2@illinois.edu>
2023-11-29 17:26:01 -0600

    add some routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import SignUpPage from './components/SignUpPage';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import SignUpPage from "./components/SignUpPage";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <div className='signup'>
-        <SignUpPage />
-      </div>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/signup" element={<SignUpPage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- my-app/src/components/MainPage/MainPage.jsx
+++ my-app/src/components/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../Header/Header.jsx";
 
 const MainPage = () => {
-    return <Header />;
+    return <Header/>;
 };
 
 export default MainPage;
