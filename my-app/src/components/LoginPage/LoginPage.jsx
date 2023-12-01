ae82822fd6ebb9325425937cd58c9f22ea59741e
haoyus2 <haoyus2@illinois.edu>
2023-12-01 15:58:45 -0600

    add link to main on SighUP and Login

--- my-app/src/components/LoginPage/LoginPage.jsx
+++ my-app/src/components/LoginPage/LoginPage.jsx
@@ -3,27 +3,40 @@ import "./LoginPage.css";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-//   const [netId, setNetId] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [confirmPassword, setConfirmPassword] = useState("");
-    return <div className="loginContainer">
-    <div className="loginInnerContainer">
-      <div className="loginTitle"> Login</div>
-      <div className="loginFormContainer">
-        <input type="text" className="login_section" placeholder="username" />
-        <input type="text" className="login_section" placeholder="password" />
-        <div className="signupIfNoAccount">
-          Don’t have an account?
-            <Link to="/signup" className='signUplink'>Sign up</Link>  
-        </div>
-        
+    //   const [netId, setNetId] = useState("");
+    //   const [password, setPassword] = useState("");
+    //   const [confirmPassword, setConfirmPassword] = useState("");
+    return (
+        <div className="loginContainer">
+            <div className="loginInnerContainer">
+                <div className="loginTitle"> Login</div>
+                <div className="loginFormContainer">
+                    <input
+                        type="text"
+                        className="login_section"
+                        placeholder="username"
+                    />
+                    <input
+                        type="text"
+                        className="login_section"
+                        placeholder="password"
+                    />
+                    <div className="signupIfNoAccount">
+                        Don’t have an account?
+                        <Link to="/signup" className="signUplink">
+                            Sign up
+                        </Link>
+                    </div>
 
-        <div className="enterButton">
-          <button type="submit">Enter</button>
+                    <div className="enterButton">
+                        <Link to="/main">
+                            <button type="submit">Enter</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>;
+    );
 };
 
 export default LoginPage;
--- my-app/src/components/SignUpPage/SignUpPage.jsx
+++ my-app/src/components/SignUpPage/SignUpPage.jsx
@@ -3,60 +3,66 @@ import "./SignUpPage.css";
 import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
-//   const [netId, setNetId] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [confirmPassword, setConfirmPassword] = useState("");
-return (
-    <div className="container">
-      <div className="innerContainer">
-        <div className="signUpTitle"> Sign Up</div>
-        <div className="uiucInformationContainer">
-          <input
-            type="text"
-            className="signup_uiuc_section"
-            placeholder="Your UIUC Net ID"
-          />
+    //   const [netId, setNetId] = useState("");
+    //   const [password, setPassword] = useState("");
+    //   const [confirmPassword, setConfirmPassword] = useState("");
+    return (
+        <div className="container">
+            <div className="innerContainer">
+                <div className="signUpTitle"> Sign Up</div>
+                <div className="uiucInformationContainer">
+                    <input
+                        type="text"
+                        className="signup_uiuc_section"
+                        placeholder="Your UIUC Net ID"
+                    />
 
-          <input
-            type="text"
-            className="signup_uiuc_section"
-            placeholder="Your UIUC password"
-          />
-        </div>
-        <div className="otherformContainer">
-        <div className="signupForm">
-            <input
-              type="text"
-              className="signup_section"
-              placeholder="Create a username"
-            />
-            <input
-              type="text"
-              className="signup_section"
-              placeholder="Create a password"
-            />
-            <input
-              type="text"
-              className="signup_section"
-              placeholder="Confirm your password"
-            />
-          </div>
-          <div className="photoUpload">
-            <label for="profile-upload">Upload <br></br>Profile Picture</label>
-            <input type="file" id="profile-upload" hidden />
-          </div>
-        </div>
-        <div className="loginIfHaveAccount">
-          Already have an account?
-          <Link to="/" className='loginInLink'>Login</Link>
-        </div>
+                    <input
+                        type="text"
+                        className="signup_uiuc_section"
+                        placeholder="Your UIUC password"
+                    />
+                </div>
+                <div className="otherformContainer">
+                    <div className="signupForm">
+                        <input
+                            type="text"
+                            className="signup_section"
+                            placeholder="Create a username"
+                        />
+                        <input
+                            type="text"
+                            className="signup_section"
+                            placeholder="Create a password"
+                        />
+                        <input
+                            type="text"
+                            className="signup_section"
+                            placeholder="Confirm your password"
+                        />
+                    </div>
+                    <div className="photoUpload">
+                        <label for="profile-upload">
+                            Upload <br></br>Profile Picture
+                        </label>
+                        <input type="file" id="profile-upload" hidden />
+                    </div>
+                </div>
+                <div className="loginIfHaveAccount">
+                    Already have an account?
+                    <Link to="/" className="loginInLink">
+                        Login
+                    </Link>
+                </div>
 
-        <div className="enterButtonSignup">
-          <button type="submit">Enter</button>
+                <div className="enterButtonSignup">
+                    <Link to="/main">
+                        <button type="submit">Enter</button>
+                    </Link>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SignUpPage;
