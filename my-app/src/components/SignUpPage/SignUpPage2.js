694ce86ebee84a595df9508010967321b75d4394
Xinyi Ai <xinyia1999@gmail.com>
2023-12-01 01:40:19 -0600

    all signup and login design

--- my-app/src/components/SignUpPage/SignUpPage2.js
+++ my-app/src/components/SignUpPage/SignUpPage2.js
@@ -10,12 +10,25 @@ function SignUpPage() {
     <div className="container">
       <div className="innerContainer">
         <div className="signUpTitle"> Sign Up</div>
-        <div className="submitInformation">
-          <div className="formContainer">
+        <div className="uiucInformationContainer">
+          <input
+            type="text"
+            className="signup_uiuc_section"
+            placeholder="Your UIUC Net ID"
+          />
+
+          <input
+            type="text"
+            className="signup_uiuc_section"
+            placeholder="Your UIUC password"
+          />
+        </div>
+        <div className="otherformContainer">
+          <div className="signupForm">
             <input
               type="text"
               className="signup_section"
-              placeholder="Your UIUC Net ID"
+              placeholder="Create a username"
             />
             <input
               type="text"
@@ -27,19 +40,20 @@ function SignUpPage() {
               className="signup_section"
               placeholder="Confirm your password"
             />
-            <div className="loginIfHaveAccount">
-              Already have an account?
-              <Link to="/">Login</Link>
-            </div>
-
-            <div className="enterButton">
-              <button type="submit">Enter</button>
-            </div>
           </div>
+
           <div className="photoUpload">
             <label for="profile-upload">Upload Profile Picture</label>
             <input type="file" id="profile-upload" hidden />
           </div>
+        </div>
+        <div className="loginIfHaveAccount">
+          Already have an account?
+          <Link to="/">Login</Link>
+        </div>
+
+        <div className="enterButton">
+          <button type="submit">Enter</button>
         </div>
       </div>
     </div>
