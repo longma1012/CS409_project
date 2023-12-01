0f10dd3ca3e6b8a6b4e8ed7f577ca5b1f6db39b7
haoyus2 <haoyus2@illinois.edu>
2023-11-30 23:16:11 -0600

    built categories

---
+++ my-app/src/components/MainPage/Categories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Categories.css";
+
+const Categories = () => {
+    return (
+        <div className="categories-container">
+            <div className="all">All</div>
+            <div className="category">Category1</div>
+            <div className="category">Category2</div>
+            <div className="category">Category3</div>
+            <div className="category">Category4</div>
+            <div className="category">Category5</div>
+        </div>
+    );
+};
+
+export default Categories;
--- my-app/src/components/MainPage/MainPage.jsx
+++ my-app/src/components/MainPage/MainPage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "../Header/Header.jsx";
+import Categories from "./Categories.jsx";
 
 const MainPage = () => {
-    return <Header/>;
+    return (
+        <div>
+            <Header />
+            <Categories />
+        </div>
+    );
 };
 
 export default MainPage;
