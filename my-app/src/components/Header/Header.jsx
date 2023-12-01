52ae97833fde1fe15a4c6d9dc819462602c294df
haoyus2 <haoyus2@illinois.edu>
2023-12-01 17:39:57 -0600

    edit color on SighUP and Login

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,31 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import tempPhoto from "../../images/Memoji Boys 2-1.png";
 import searchIcon from "../../images/SearchIcon.png";
 import Logo from "../../images/ProjectLogo.png";
 import "./Header.css";
 
-
 const Header = () => {
     return (
         <div className="header-container">
-            <div className="home">
-                <div className="logo-container">
-                    <img src={Logo} alt=""  className="logo-image" />
+            <Link to="/main" className="home-link">
+                <div className="home">
+                    <div className="logo-container">
+                        <img src={Logo} alt="" className="logo-image" />
+                    </div>
+                    <header className="web-name">I-AlumniHub</header>
                 </div>
-                <header className="web-name">I-AlumniHub</header>
-            </div>
+            </Link>
             <div className="search-bar">
-                <input
-                    type="text"
-                    placeholder="Type here to search..."
-                />
+                <input type="text" placeholder="Type here to search..." />
                 <div className="search-icon">
                     <img src={searchIcon} alt="" />
                 </div>
             </div>
             <div className="User">
                 <div className="photo-container">
-                    <img src={tempPhoto} alt="" className="photo"/>
+                    <img src={tempPhoto} alt="" className="photo" />
                 </div>
                 <p className="username">Username123</p>
             </div>
--- my-app/src/components/MainPage/MainPage.jsx
+++ my-app/src/components/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Header/Header.jsx";
 import Categories from "./Categories.jsx";
+import "./MainPage.css";
 
 import tempPhoto from "../../images/Memoji Boys 2-1.png";
 
@@ -9,18 +10,20 @@ const MainPage = () => {
         <div>
             <Header />
             <Categories />
-            <div className="CreatePost">
-                <div className="user-photo">
-                    <img src={tempPhoto} alt="" className="photo" />
-                </div>
-                <div className="create-post-input">
-                    <input
-                        type="text"
-                        placeholder="Let's share what going on your mind..."
-                    />
-                </div>
-                <div className="create-post-button">
-                    <button>Create Post</button>
+            <div className="main-area">
+                <div className="CreatePost">
+                    <div className="user-photo">
+                        <img src={tempPhoto} alt="" className="photo" />
+                    </div>
+                    <div className="create-post-input">
+                        <input
+                            type="text"
+                            placeholder="Let's share what going on your mind..."
+                        />
+                    </div>
+                    <div className="create-post-button">
+                        <button>Create Post</button>
+                    </div>
                 </div>
             </div>
         </div>
