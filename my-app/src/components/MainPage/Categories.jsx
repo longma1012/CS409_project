1a82dead9ecc03f20fc53c2189d8cbcf1ddb11f4
haoyus2 <haoyus2@illinois.edu>
2023-12-02 03:49:57 -0600

    edit categories

--- my-app/src/components/MainPage/Categories.jsx
+++ my-app/src/components/MainPage/Categories.jsx
@@ -6,8 +6,8 @@ const Categories = () => {
         <div className="categories-container">
             <div className="all">All</div>
             <div className="category">Alumni Events</div>
-            <div className=" category">Lifestyle and Hobbies</div>
-            <div className="category">Job and Career</div>
+            <div className=" category">Lifestyle & Hobbies</div>
+            <div className="category">Job & Career</div>
             <div className="category">Food & Drink</div>
             <div className="category">Academic Discussions</div>
             <div className="category">Food & Drink</div>
