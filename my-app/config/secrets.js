9c119d07ce8bdadd3e93f528e8d38d34cf1a3997
haoyus2 <haoyus2@illinois.edu>
2023-12-08 15:29:50 -0600

    firebase installed

--- my-app/config/secrets.js
+++
@@ -1,8 +0,0 @@
-/*
- * You generally want to .gitignore this file to prevent important credentials from being stored on your public repo.
- */
-module.exports = {
-    token : "secret-starter-mern",
-    mongo_connection : "mongodb+srv://longma2:zhuoying45@cluster0.5ffkrqc.mongodb.net/?retryWrites=true&w=majority"
-    //example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours-web-provided].mongodb.net/test?retryWrites=true"
-};
