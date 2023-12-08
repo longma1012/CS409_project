b6b4617b34e5bf986ad341a13551c1b4fa91ccaa
haoyus2 <haoyus2@illinois.edu>
2023-12-07 23:39:06 -0600

    comment bug fixed

--- my-app/src/components/ViewPost/ViewPost.jsx
+++ my-app/src/components/ViewPost/ViewPost.jsx
@@ -9,176 +9,185 @@ import { useParams, useLocation } from "react-router-dom";
 import { readPostData, updatePost } from "../../dbUtils/CRUDPost.js";
 import { readUsename } from "../../dbUtils/CRUDUser";
 import {
-  writeCommentData,
-  readCommentData,
+    writeCommentData,
+    readCommentData,
 } from "../../dbUtils/CRUDComment.js";
 import { v4 as uuidv4 } from "uuid";
 
 const ViewPost = () => {
-  const [postUsername, setPostUsername] = useState("");
-  const [myUsername, setMyUsername] = useState("");
-  const [comments, setComments] = useState([]); // Add this state to hold comment data
-  const [myUserId, setMyUserId] = useState("");
-  const [comment, setComment] = useState("");
-  const [post, setPost] = useState("");
-  const { postId } = useParams();
+    const [postUsername, setPostUsername] = useState("");
+    const [myUsername, setMyUsername] = useState("");
+    const [comments, setComments] = useState([]); // Add this state to hold comment data
+    const [myUserId, setMyUserId] = useState("");
+    const [comment, setComment] = useState("");
+    const [post, setPost] = useState("");
+    const { postId } = useParams();
 
-  // handle go back Btn
-  const location = useLocation();
-  const handleGoBack = () => {
-    window.history.back();
-  };
-  const handleCommentChange = (e) => {
-    setComment(e.target.value);
-  };
-
-  const submitComment = () => {
-    const commentId = uuidv4(); // Generate a unique ID for the comment
-    const createTime = new Date().toISOString();
-    const newComment = {
-      Content: comment,
-      CreateTime: createTime,
-      CommentUserName: myUsername,
-      id: commentId, // Assuming the comment object has an 'id' field
+    // handle go back Btn
+    const location = useLocation();
+    const handleGoBack = () => {
+        window.history.back();
+    };
+    const handleCommentChange = (e) => {
+        setComment(e.target.value);
     };
 
-    // Write the new comment data to the database
-    writeCommentData(commentId, postId, comment, createTime, myUsername);
-
-    // Update the post's CommentList with the new comment ID
-    const updatedCommentList = post.CommentList
-      ? [...post.CommentList, commentId]
-      : [commentId];
-    updatePost(postId, { CommentList: updatedCommentList });
-
-    // Add the new comment to the comments state to update the UI immediately
-    setComments([...comments, newComment]);
-
-    // Clear the comment input after submitting
-    setComment("");
-  };
-
-  // get post user name
-  useEffect(() => {
-    const fetchPostUsername = async () => {
-      try {
-        console.log(post.userId);
-        const fetchedUsername = await readUsename(post.userId);
-        setPostUsername(fetchedUsername || "Unknown User");
-      } catch (error) {
-        console.error("Error fetching username:", error);
-      }
-    };
-    fetchPostUsername();
-  }, [post.userId]);
-
-  // get my user name
-  useEffect(() => {
-    const fetchMyUsername = async () => {
-      try {
-        const fetchedMyUsername = await readUsename(myUserId);
-        // console.log("myuserid  " + fetchedMyUsername);
-        setMyUsername(fetchedMyUsername || "Unknown User");
-      } catch (error) {
-        console.error("Error fetching username:", error);
-      }
-    };
-    fetchMyUsername();
-  }, [myUserId]);
-
-  useEffect(() => {
-    // Fetch post data when postId changes
-    if (postId) {
-      readPostData(postId, (postData) => {
-        setPost(postData);
-      });
-    }
-  }, [postId]); // This effect runs when postId changes
-
-  useEffect(() => {
-    // Fetch post data when postId changes
-    if (postId) {
-      readPostData(postId, (postData) => {
-        setPost(postData);
-        if (postData.CommentList) {
-          // For each comment ID in the CommentList, fetch the comment data
-          const commentPromises = postData.CommentList.map(
-            (commentId) =>
-              new Promise((resolve) => {
-                readCommentData(commentId, (commentData) => {
-                  resolve({ id: commentId, ...commentData });
-                });
-              })
-          );
-          Promise.all(commentPromises).then((commentsData) => {
-            setComments(commentsData); // Update the comments state with all fetched comments
-          });
+    const handleCommentSubmit = () => {
+        if (myUsername && post && comment) {
+            submitComment();
+        } else {
+            console.log("Username or post information is not available.");
         }
-      });
-    }
-  }, [postId]);
+    };
 
-  useEffect(() => {
-    // let userId = "";
-    const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      // console.log(user);
-      if (user) {
-        setMyUserId(user.uid);
-      } else {
-        setMyUserId("");
-      }
-    });
-    return () => unsubscribe();
-  }, []);
+    const submitComment = () => {
+        if (myUsername == "Unknown User") {
+            console.log("User not logged in");
+            return;
+        }
+        const commentId = uuidv4();
+        const createTime = new Date().toISOString();
+        const newComment = {
+            Content: comment,
+            CreateTime: createTime,
+            CommentUserName: myUsername,
+            id: commentId,
+        };
 
-  return (
-    <>
-      <Header />
-      <div className={styles.viewPostContainer}>
-        <div className={styles.backBtnContainer}>
-          <button className={styles.backBtn} onClick={handleGoBack}>
-            Back
-          </button>
-        </div>
-        <div className={styles.postDetails}>
-          <div className={styles.postTitle}> {post.Title} </div>
-          <div className={styles.name}> {postUsername} </div>
-          <div className={styles.postContent}>{post.Body}</div>
-          <div className={styles.categoryAndLikes}>
-            <div className={styles.category}>{post.Category}</div>
-            <div className={styles.likeArea}>
-              <div className={styles.likeBtn}>
-                <img src={LikeIcon} alt="" />
-              </div>
-              <div className={styles.likes}>{post.Likes}</div>
+        // Write the new comment data to the database
+        writeCommentData(commentId, postId, comment, createTime, myUsername);
+
+        // Update the post's CommentList with the new comment ID
+        const updatedCommentList = post.CommentList
+            ? [...post.CommentList, commentId]
+            : [commentId];
+        updatePost(postId, { CommentList: updatedCommentList });
+
+        // Add the new comment to the comments state to update the UI immediately
+        setComments([...comments, newComment]);
+
+        // Clear the comment input after submitting
+        setComment("");
+    };
+
+    // get post user name
+    useEffect(() => {
+        const fetchPostUsername = async () => {
+            try {
+                // console.log(post.userId);
+                if (post.userId) {
+                    const fetchedUsername = await readUsename(post.userId);
+                    setPostUsername(fetchedUsername || "Unknown User");
+                }
+            } catch (error) {
+                console.error("Error fetching username:", error);
+            }
+        };
+        fetchPostUsername();
+    }, [post.userId]);
+
+    // get my user name
+    useEffect(() => {
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                setMyUserId(user.uid);
+                try {
+                    const fetchedMyUsername = await readUsename(user.uid);
+                    setMyUsername(fetchedMyUsername || "Unknown User");
+                } catch (error) {
+                    console.error("Error fetching username:", error);
+                }
+            } else {
+                setMyUserId("");
+                setMyUsername("Unknown User");
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+
+    useEffect(() => {
+        // Fetch post data when postId changes
+        if (postId) {
+            readPostData(postId, (postData) => {
+                setPost(postData);
+            });
+        }
+    }, [postId]); // This effect runs when postId changes
+
+    useEffect(() => {
+        // Fetch post data when postId changes
+        if (postId) {
+            readPostData(postId, (postData) => {
+                setPost(postData);
+                if (postData.CommentList) {
+                    // For each comment ID in the CommentList, fetch the comment data
+                    const commentPromises = postData.CommentList.map(
+                        (commentId) =>
+                            new Promise((resolve) => {
+                                readCommentData(commentId, (commentData) => {
+                                    resolve({ id: commentId, ...commentData });
+                                });
+                            })
+                    );
+                    Promise.all(commentPromises).then((commentsData) => {
+                        setComments(commentsData); // Update the comments state with all fetched comments
+                    });
+                }
+            });
+        }
+    }, [postId]);
+
+    return (
+        <>
+            <Header />
+            <div className={styles.viewPostContainer}>
+                <div className={styles.backBtnContainer}>
+                    <button className={styles.backBtn} onClick={handleGoBack}>
+                        Back
+                    </button>
+                </div>
+                <div className={styles.postDetails}>
+                    <div className={styles.postTitle}> {post.Title} </div>
+                    <div className={styles.name}> {postUsername} </div>
+                    <div className={styles.postContent}>{post.Body}</div>
+                    <div className={styles.categoryAndLikes}>
+                        <div className={styles.category}>{post.Category}</div>
+                        <div className={styles.likeArea}>
+                            <div className={styles.likeBtn}>
+                                <img src={LikeIcon} alt="" />
+                            </div>
+                            <div className={styles.likes}>{post.Likes}</div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.selfCommentPost}>
+                    <input
+                        type="text"
+                        placeholder="Leave your content here..."
+                        value={comment}
+                        onChange={handleCommentChange}
+                    />
+                    <button onClick={handleCommentSubmit}>Comment</button>
+                </div>
+                <div className={styles.postComments}>
+                    {comments.map((singlecomment) => (
+                        <div
+                            key={singlecomment.id}
+                            className={styles.postComment}
+                        >
+                            <div className={styles.commenterID}>
+                                {singlecomment.CommentUserName}
+                            </div>
+                            <div className={styles.commentContent}>
+                                {singlecomment.Content}
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          </div>
-        </div>
-        <div className={styles.selfCommentPost}>
-          <input
-            type="text"
-            placeholder="Leave your content here..."
-            value={comment}
-            onChange={handleCommentChange}
-          />
-          <button onClick={submitComment}>Comment</button>
-        </div>
-        <div className={styles.postComments}>
-          {comments.map((singlecomment) => (
-            <div key={singlecomment.id} className={styles.postComment}>
-              <div className={styles.commenterID}>
-                {singlecomment.CommentUserName}
-              </div>
-              <div className={styles.commentContent}>
-                {singlecomment.Content}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default ViewPost;
