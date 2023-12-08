ee986ac9d4fa48a851209a175162dab9dca1e87c
Xinyi Ai <xinyia1999@gmail.com>
2023-12-07 21:44:19 -0600

    ahahaha

--- my-app/src/components/MainPage/PostCard.jsx
+++ my-app/src/components/MainPage/PostCard.jsx
@@ -1,4 +1,4 @@
-import React , { useEffect, useState }from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./PostCard.module.css";
 import { readUsename } from "../../dbUtils/CRUDUser";
@@ -8,18 +8,20 @@ import tempPhoto from "../../images/Memoji Boys 2-1.png";
 const PostCard = ({ post }) => {
   console.log(post);
   // count comment numbers
-  const commentCount = post.CommentList ? Object.keys(post.CommentList).length : 0;
-  
+  const commentCount = post.CommentList
+    ? Object.keys(post.CommentList).length
+    : 0;
+
   // get poster name
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   useEffect(() => {
     const fetchUsername = async () => {
       try {
-        console.log(post.userId)
+        console.log(post.userId);
         const fetchedUsername = await readUsename(post.userId);
-        setUsername(fetchedUsername || 'Unknown User');
+        setUsername(fetchedUsername || "Unknown User");
       } catch (error) {
-        console.error('Error fetching username:', error);
+        console.error("Error fetching username:", error);
       }
     };
 
@@ -31,30 +33,30 @@ const PostCard = ({ post }) => {
     const currentTime = new Date();
     const postDate = new Date(postTime);
     const difference = currentTime - postDate;
-  
+
     // 转换为秒
     const seconds = Math.floor(difference / 1000);
-  
+
     if (seconds < 60) {
-      return 'Just now';
+      return "Just now";
     } else if (seconds < 3600) {
       const minutes = Math.floor(seconds / 60);
-      return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
+      return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
     } else if (seconds < 86400) {
       const hours = Math.floor(seconds / 3600);
-      return `${hours} hour${hours !== 1 ? 's' : ''} ago`;
-    } else if (seconds < 2592000){
+      return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
+    } else if (seconds < 2592000) {
       const days = Math.floor(seconds / 86400);
-      return `${days} day${days !== 1 ? 's' : ''} ago`;
+      return `${days} day${days !== 1 ? "s" : ""} ago`;
     } else if (seconds < 31536000) {
       const months = Math.floor(seconds / 2592000);
-      return `${months} month${months !== 1 ? 's' : ''} ago`;
+      return `${months} month${months !== 1 ? "s" : ""} ago`;
     } else {
       const years = Math.floor(seconds / 31536000);
-      return `${years} year${years !== 1 ? 's' : ''} ago`;
+      return `${years} year${years !== 1 ? "s" : ""} ago`;
     }
   };
-  
+
   // 使用 getTimeDifference 函数计算时间差距
   const formattedTimeDifference = getTimeDifference(post.PostTime);
 
@@ -63,14 +65,8 @@ const PostCard = ({ post }) => {
       <div className={styles.content_area}>
         <div className={styles.post_card_title}>{post.Title}</div>
         <div className={styles.poster_area}>
-<<<<<<< HEAD
-          {/* TODO change post.userid */}
-          <p className={styles.poster_name}>{post.UserEmail}</p>
-          <p className={styles.post_time}>{post.PostTime}</p>
-=======
           <p className={styles.poster_name}>{username}</p>
           <p className={styles.post_time}>{formattedTimeDifference}</p>
->>>>>>> aa77038bb4beefa6c056d667cf5d33d66e2fa4ec
         </div>
         <div className={styles.post_card_content}>{post.Body}</div>
       </div>
